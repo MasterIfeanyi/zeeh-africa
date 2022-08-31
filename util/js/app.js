6aaaca11a0402537a5b94d7d28b6374be1c740cb
Ifeanyi <chimaifeanyi29@gmail.com>
2022-08-31 15:39:43 +0100

    first commit

---
+++ util/js/app.js
@@ -0,0 +1,53 @@
+// portfolio
+$('#tech-carousel').owlCarousel({
+    loop:true,
+    margin:48,
+    dots:true,
+    nav: false,
+    items: 1,
+    smartSpeed: 1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:2
+    //     },
+    //     1000:{
+    //         items: 3
+    //     }
+    // }
+})
+
+
+// autoplayHoverPause, pause auto play on hover 
+
+
+
+// Reviews
+$('#why-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav: false,
+    smartSpeed: 1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+// autoplayHoverPause, pause auto play on hover 
